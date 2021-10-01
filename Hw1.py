c7472a9f4ce56237dcdcec83f3641f5e3bdd9d30
ym1769403 <70408430+ym1769403@users.noreply.github.com>
2021-09-30 20:00:56 -0500

    created menu and if/elif  options

--- Hw1.py
+++ Hw1.py
@@ -2,8 +2,10 @@ import mysql.connector
 from mysql.connector import Error
 import datetime
 from datetime import date 
-today = date.today()
-#https://stackabuse.com/how-to-format-dates-in-python/ 
+
+today = date.today() # used to find the current date later used  when adding item in the menu
+#https://stackabuse.com/how-to-format-dates-in-python/  Source used 
+
 def create_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
@@ -41,26 +43,13 @@ def execute_read_query(connection, query):
 
 
 conn = create_connection("cis3368.cejnvvmjqiep.us-east-2.rds.amazonaws.com", "admin", "freg2784", "cis3368fall2021")  #established successful connection with database conn is the connection object
-
-
-# use data and how to sort records(find sorting algorithm and cite/explain) for this assignment 
-
-
-cursor = conn.cursor(dictionary=True)
+ 
+cursor = conn.cursor(dictionary=True) #to format it as a dictionary
 sql = "SELECT * FROM shoppinglist"
 cursor.execute(sql)
 rows = cursor.fetchall()
-
-
-def slist():
-    cursor = conn.cursor(dictionary=True)
-    sql = "SELECT * FROM shoppinglist"
-    cursor.execute(sql)
-    rows = cursor.fetchall()
-    for items in rows:  
-         print(items)  
-
-#slist()     # this returns the whole table made
+for items in rows:
+    print(items)
 
 #create the menu
 def menu():
@@ -70,53 +59,46 @@ def menu():
     print('r1 - Output all items in alphabetical order')
     print('r2- Output all items by sorted by quantity (ascending)')
     print('q - Quit') 
-
-menu() #this returns the menu
-option = int(input('Enter your option'))
-
-if option == 'a':
-    add_id = ''
-    add_item_description = ''
-    add_quantity = ''
-    add_current_date = today     #reference current date added 
-    query = "INSERT INTO slist (id, itemdescription, quantity,dateadded) VALUES (%s, '%s', %s, %s)" % (add_id, add_item_description, add_quantity, add_current_date)
-    #execute_query(conn, query)
+    option = input('Enter your option')
+    while True:
+        
+        if option == 'a':
+            add_id = input('Enter id')
+            add_item_description = input('Enter item description')
+            add_quantity =  input('Enter item quantity')
+            add_current_date = today     #reference current date added 
+            query = "INSERT INTO shoppinglist (id, itemdescription, quantity,dateadded) VALUES (%s, '%s', %s, %s)" % (add_id, add_item_description, add_quantity, add_current_date)
+            execute_query(conn, query)    
     
-elif option == 'd':
-    item_to_delete = ''
-    delete_statement = "DELETE FROM slist WHERE id = %s" % (item_to_delete)   
-    #execute_query(conn, delete_statement)
+        elif option == 'd':
+            item_to_delete = input('Enter item to delete')
+            delete_statement = "DELETE FROM shoppinglist WHERE id = %s" % (item_to_delete)   
+            execute_query(conn, delete_statement)
+ 
+        elif option == 'u':
+            updated_item_description = input('Enter item to update') #update here
+            update_shopping_list = """
+            UPDATE shoppinglist
+            SET amount = %s
+            WHERE id = %s """ % (updated_item_description) 
+            execute_query(conn, update_shopping_list)
+
+        elif option == 'r1':    #https://www.w3schools.com/python/python_mysql_orderby.asp  Source used to help with the sorting alphhabeitcally for r1 and r2 
+            sql_item = "SELECT * FROM shoppinglist ORDER BY itemdescription"
+            execute_query(conn,sql_item)    
     
-elif option == 'u':
-    updated_item_description = '' #update here
-    update_shopping_list = """
-    UPDATE slist
-    SET amount = %s
-    WHERE id = 1 """ % (updated_item_description) 
-    #execute_query(conn, update_shopping_list)
+        elif option == 'r2':
+            sql_quantity = "SELECT * FROM shoppinglist ORDER BY quantity"
+            execute_query(conn,sql_quantity)
     
-#elif option == 'r1':
-        #
-    
+        elif option == 'q':
+            break #to quit
 
-#elif option == 'r2':
-    #
-
-#elif option == 'q':
-    #break #to quit
+menu()  #to output the menu
 
 
 
 
 
-
-
-
-#this is how the table in sql is called(use for reference  idk  if needed )
-
-#cart = "SELECT * FROM shoppinglist"
-#list = execute_read_query(conn, cart)
-#for cart in list:
-    #print(cart) #ill use this later but without the print
 
 
